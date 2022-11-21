9649af986f75ee6d393c3ce01854cfd71b9e011e
kiranvm143 <kiranmarkad3@gmail.com>
2022-11-21 17:40:10 +0530

    Continue dataTablePractice editing

--- force-app/main/default/lwc/dataTablePractice/dataTablePractice.js
+++ force-app/main/default/lwc/dataTablePractice/dataTablePractice.js
@@ -1,3 +1,34 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import GET_CONTACTS from '@salesforce/apex/WireContactPractice.ContactPractice';
 
-export default class DataTablePractice extends LightningElement {}
+export default class DataTablePractice extends LightningElement {
+
+ actions =[
+    { label:'View', name:'view' },
+    { label:'View', name:'view' },
+    { label:'View', name:'view' }
+];
+
+@track columns =[
+    { title:'Name', fieldName:'Name'},
+    { title:'Email', fieldName:'Email'},
+    { title:'Phone', fieldName:'Phone'},
+    { title:'Title', fieldName:'Title'},
+    { type:'action',
+        typeAttributes:{ rowActions:this.actions}
+    }
+    
+];
+
+@wire(GET_CONTACTS)
+contactWire (result) {
+    if (result && result.data ) {
+        // TODO: Error handling
+    } else if (result && result.data) {
+        // TODO: Data handling
+    }
+}
+
+
+
+}
