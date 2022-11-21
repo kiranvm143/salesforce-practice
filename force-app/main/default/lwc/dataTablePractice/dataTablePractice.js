4cf587cb151aecd9e91ad7e8de05fe08cb1114ea
kiranvm143 <kiranmarkad3@gmail.com>
2022-11-21 23:41:50 +0530

    Actions-Check

--- force-app/main/default/lwc/dataTablePractice/dataTablePractice.js
+++ force-app/main/default/lwc/dataTablePractice/dataTablePractice.js
@@ -1,33 +1,113 @@
 import { LightningElement, track, wire } from 'lwc';
 import GET_CONTACTS from '@salesforce/apex/WireContactPractice.ContactPractice';
+import {NavigationMixin} from 'lightning/navigation';
+//import {deleteRecord} from 'lightning/uiRecordApi';
+//import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 
-export default class DataTablePractice extends LightningElement {
 
- actions =[
-    { label:'View', name:'view' },
-    { label:'View', name:'view' },
-    { label:'View', name:'view' }
-];
-
-@track columns =[
-    { title:'Name', fieldName:'Name'},
-    { title:'Email', fieldName:'Email'},
-    { title:'Phone', fieldName:'Phone'},
-    { title:'Title', fieldName:'Title'},
-    { type:'action',
-        typeAttributes:{ rowActions:this.actions}
-    }
+export default class DataTablePractice extends NavigationMixin(LightningElement) {
     
-];
+   // selectedId;
+    //isModal=false;
+    @track data;
+    actions =[
+        { label:'View', name:'view' },
+        { label:'Edit', name:'edit' },
+        { label:'Delete', name:'delete' }
+        ];
 
-@wire(GET_CONTACTS)
-contactWire (result) {
-    if (result && result.data ) {
-        // TODO: Error handling
-    } else if (result && result.data) {
-        // TODO: Data handling
+    @track columns =[
+        { label:'Name', fieldName:'Name'},
+        { label:'Email', fieldName:'Email'},
+        { label:'Phone', fieldName:'Phone'},
+        { label:'Title', fieldName:'Title'},
+        { type:'action',
+        typeAttributes:{ rowActions:this.actions}
+        }
+    
+    ];
+
+    @wire(GET_CONTACTS)
+    contactWire (result) {
+       
+       
+        if (result && result.data ) {
+        this.data = result.data;
+        } else if (result && result.error) {
+        // TODO: error handling
+        }
     }
-}
+
+    rowActionEvent(event){
+        console.log({...event.detail.row});
+        let actionName= event.detail.action.name;
+        
+        let Id =event.detail.row.Id;
+       //this.selectedId = Id;
+
+
+       if(actionName== 'view' || actionName == 'edit'){
+            this.navigationMixinAction(actionName,Id);
+        }
+        else if(actionName == 'delete'){
+           // this.confirmDelete();
+        }
+
+
+    }
+
+    navigationMixinAction(action, Id){
+        this[NavigationMixin.Navigate]({
+            type:'standard__recordPage',
+            attributes:{
+                recordId:Id,
+                actionName:action,
+                objectApiName: 'Contact'
+
+            },
+        });
+    }
+
+   /* deleteRecord(){
+        this.closeModal();
+        deleteRecord(this.selectedId)
+        .then(()=>{
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title:'Success',
+                    message:'Record Deleted Successfully',
+                    variant:'success'
+
+                })
+            );
+           
+
+
+        }
+        )
+        .catch(error =>{
+            this.dispatchEvent(
+                new ShowToastEvent(
+                    {
+                        title:'Error',
+                        message: error.body.message,
+                        variant: 'error'
+
+                    }
+                )
+            );
+
+        });
+    }
+
+    confirmDelete(){
+        this.isModal=true;
+    }
+
+    closeModal(){
+        this.isModal=false;
+    }
+*/
 
 
 
